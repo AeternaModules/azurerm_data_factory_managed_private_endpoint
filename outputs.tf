a79f0eb150305c8df26788936d174760c4f496f7
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:40:18 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "data_factory_managed_private_endpoints" {
+  description = "All data_factory_managed_private_endpoint resources"
+  value       = azurerm_data_factory_managed_private_endpoint.data_factory_managed_private_endpoints
+}
+
