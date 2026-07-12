1a4526e9cf80fd757c00cc2d3ba4c2ede9647c76
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:57 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "data_factory_managed_private_endpoints" {
-  description = "All data_factory_managed_private_endpoint resources"
-  value       = azurerm_data_factory_managed_private_endpoint.data_factory_managed_private_endpoints
-}
 output "data_factory_managed_private_endpoints_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_managed_private_endpoints"
-  value       = [for k, v in azurerm_data_factory_managed_private_endpoint.data_factory_managed_private_endpoints : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_managed_private_endpoints, keyed the same as var.data_factory_managed_private_endpoints"
+  value       = { for k, v in azurerm_data_factory_managed_private_endpoint.data_factory_managed_private_endpoints : k => v.data_factory_id }
 }
 output "data_factory_managed_private_endpoints_fqdns" {
-  description = "List of fqdns values across all data_factory_managed_private_endpoints"
-  value       = [for k, v in azurerm_data_factory_managed_private_endpoint.data_factory_managed_private_endpoints : v.fqdns]
+  description = "Map of fqdns values across all data_factory_managed_private_endpoints, keyed the same as var.data_factory_managed_private_endpoints"
+  value       = { for k, v in azurerm_data_factory_managed_private_endpoint.data_factory_managed_private_endpoints : k => v.fqdns }
 }
 output "data_factory_managed_private_endpoints_name" {
-  description = "List of name values across all data_factory_managed_private_endpoints"
-  value       = [for k, v in azurerm_data_factory_managed_private_endpoint.data_factory_managed_private_endpoints : v.name]
+  description = "Map of name values across all data_factory_managed_private_endpoints, keyed the same as var.data_factory_managed_private_endpoints"
+  value       = { for k, v in azurerm_data_factory_managed_private_endpoint.data_factory_managed_private_endpoints : k => v.name }
 }
 output "data_factory_managed_private_endpoints_subresource_name" {
-  description = "List of subresource_name values across all data_factory_managed_private_endpoints"
-  value       = [for k, v in azurerm_data_factory_managed_private_endpoint.data_factory_managed_private_endpoints : v.subresource_name]
+  description = "Map of subresource_name values across all data_factory_managed_private_endpoints, keyed the same as var.data_factory_managed_private_endpoints"
+  value       = { for k, v in azurerm_data_factory_managed_private_endpoint.data_factory_managed_private_endpoints : k => v.subresource_name }
 }
 output "data_factory_managed_private_endpoints_target_resource_id" {
-  description = "List of target_resource_id values across all data_factory_managed_private_endpoints"
-  value       = [for k, v in azurerm_data_factory_managed_private_endpoint.data_factory_managed_private_endpoints : v.target_resource_id]
+  description = "Map of target_resource_id values across all data_factory_managed_private_endpoints, keyed the same as var.data_factory_managed_private_endpoints"
+  value       = { for k, v in azurerm_data_factory_managed_private_endpoint.data_factory_managed_private_endpoints : k => v.target_resource_id }
 }
 
